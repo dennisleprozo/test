6c28ad704bed6b7c99e1aafc77cf148a4ff5a36b
dennisleprozo <dennisvleprozo@gmail.com>
2018-12-19 11:04:20 -0700

    deleted logo add routes

--- testproject/src/App.js
+++ testproject/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import MainMenu from './components/MainMenu/MainMenu'
+import MainMenu from './components/MainMenu/MainMenu' 
 
 class App extends Component {
   render() {
