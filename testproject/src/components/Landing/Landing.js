447f88dcd8f4adc688259d1a94f187a57a6525d4
dennisleprozo <dennisvleprozo@gmail.com>
2019-01-05 22:04:58 -0700

    edit landing page

--- testproject/src/components/Landing/Landing.js
+++ testproject/src/components/Landing/Landing.js
@@ -9,11 +9,26 @@ class Landing extends Component {
            {/* Carousel */}
            <Carousel>
                <Carousel.Item>
+
                   <div className="carousel-item" responsive='true' style={{backgroundImage: 'url(Images/ems.jpg)'}} />
                      <Carousel.Caption>
                         <h1>DEVIANT APPAREL</h1>
                     </Carousel.Caption>
+
                </Carousel.Item>
+
+
+               <Carousel.Item>
+                  <img width={900} height={500} alt="900x500" src="https://images.unsplash.com/photo-1542838077-7fb322b21b80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+             
+               </Carousel.Item>
+
+
+
+
+
+
+               
                <Carousel.Item>
                   <div className="carousel-item" responsive='true' style={{backgroundImage: 'url(Images/yeezy-boost-700-mauve-wallpaper-4k.jpg)'}} />
                      <Carousel.Caption>
